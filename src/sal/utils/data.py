abd31a97abd7b6ead850964531fb36b047f271ed
Alfred Nguyen <avocadoaling@gmail.com>
2025-02-07 14:12:10 +0100

    change: temperature in output files for completion

--- src/sal/utils/data.py
+++ src/sal/utils/data.py
@@ -71,10 +71,10 @@ def save_dataset(dataset, config):
         if config.output_dir is None:
             config.output_dir = f"data/{config.model_path}"
         Path(config.output_dir).mkdir(parents=True, exist_ok=True)
-        output_file = Path(f"{config.output_dir}/{config.approach}_completions.jsonl")
+        output_file = Path(f"{config.output_dir}/{config.approach}_completions_temp_{config.temperature}.jsonl")
         counter = 1
         while output_file.exists():
-            output_file = Path(f"{config.output_dir}/{config.approach}_completions-{counter}.jsonl")
+            output_file = Path(f"{config.output_dir}/{config.approach}_completions_temp_{config.temperature}-{counter}.jsonl")
             counter += 1
 
         dataset.to_json(output_file, lines=True)
